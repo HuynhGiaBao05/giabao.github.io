ef4addebd91ff3be7debec26a614b2bba670c6ee
HuynhGiaBao05 <giabaohuynh10905@gmail.com>
2025-08-04 13:16:05 +0700

    cập nhật giao diện profile

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ document.addEventListener('DOMContentLoaded', function () {
       },
     });
   }
+});
 
   // ======================= Toggle Menu =======================
   const menu = document.querySelector('#menu-icon');
